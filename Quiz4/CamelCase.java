17aa3c5e298aafd0695f850da4c655b74cfc6929
Nicholas Johnson <104460821+Vannicked@users.noreply.github.com>
2023-10-04 13:04:47 -0400

    Create CamelCase.java

---
+++ Quiz4/CamelCase.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'camelcase' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int camelcase(String s) {
+      // Check if the string is empty.
+    if (s.isEmpty()) {
+        return 1;
+    }
+
+    // Count the number of uppercase characters in the string.
+    int count = 1;
+    for (int i = 1; i < s.length(); i++) {
+        if (Character.isUpperCase(s.charAt(i))) {
+            count++;
+        }
+    }
+
+    // Return the number of words in the string, which is equal to the number of uppercase characters plus one.
+    return count;
+}
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        int result = Result.camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
